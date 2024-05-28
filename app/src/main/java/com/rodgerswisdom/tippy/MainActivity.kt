83a4cc66b74ad7e55e65516bd33cacd77f4143df
rodgerswisdom <trulyhawona@gmail.com>
2024-05-28 22:49:31 +0300

    Declare lateint private  variables

--- app/src/main/java/com/rodgerswisdom/tippy/MainActivity.kt
+++ app/src/main/java/com/rodgerswisdom/tippy/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.rodgerswisdom.tippy
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etBaseAmount: EditText
+    private lateinit var tvPercentageLabel: TextView
+    private lateinit var etSeekBarTip: SeekBar
+    private lateinit var tipAmount: TextView
+    private lateinit var totalAmount: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
